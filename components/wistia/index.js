eed767bfcfa4499c1b129191db70767f524485ef
Rushikesh Shinde <rushidshinde@gmail.com>
2026-08-07 19:38:34 +0530

    feat: add Wistia component with dynamic style injection

---
+++ components/wistia/index.js
@@ -0,0 +1,2 @@
+import{i}from"../../assets/scripts/insert-styles-43b34cac.js";var t=`[data-component-id=wistia] .w-embed.w-script,[data-component-id=wistia] .w-embed.w-script .wistia_responsive_padding{height:100%}
+`;i(t);
