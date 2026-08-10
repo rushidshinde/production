441c3a777dba37c1bcc51b9c55b536f181ac303f
Rushikesh Shinde <rushidshinde@gmail.com>
2026-08-10 15:16:20 +0530

    feat: add share, article anchors, external rich link, and copy link components to production directory

---
+++ components/article-rich-link-new-tab/index.js
@@ -0,0 +1,1 @@
+var r='[data-component-id="article-rich-link-new-tab"]',n=function(){var t=document.querySelector(r),i=t.querySelectorAll("a");i.forEach(function(e){e.setAttribute("target","_blank")})};n();
